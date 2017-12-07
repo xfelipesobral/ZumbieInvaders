ab2162f1477bd2e20cb4a765315d1a237d975485
LuanUmeres <33167481+LuanUmeres@users.noreply.github.com>
2017-12-06 23:39:15 -0200

    Adição de testes

--- teste_main.py
+++ teste_main.py
@@ -14,11 +14,42 @@ class MeusTestes(unittest.TestCase):
     def teste_moverZumbi(self):
 
         # MOVER ZUMBI PARA DIREITA
-        self.assertEqual(mover_zumbi(Zumbi(PAREDE_ESQUERDA, PAREDE_ESQUERDA, DZ), Zumbi(PAREDE_ESQUERDA, PAREDE_ESQUERDA, DZ)+DZ, DZ))
+        self.assertEqual(mover_zumbi(Zumbi(PAREDE_ESQUERDA, PAREDE_ESQUERDA, DZ), Zumbi(PAREDE_ESQUERDA, PAREDE_ESQUERDA+DZ, DZ)))
 
-        # ZUMBI CAINDO NO Y
+        #MOVER P/ DIREITA E DESCER
+        self.assertEqual(mover_zumbi(Zumbi(PAREDE_ESQUERDA, PAREDE_ESQUERDA, DZ), Zumbi(PAREDE_DIREITA, PAREDE_DIREITA+DESLOCAMENTO_ZUMBI, DZ)))
 
-        ### SELF            FUNC_T              OBJETO SENDO TESTADO                             RESULTADO ESPERADO
-        self.assertEqual(mover_zumbi(Zumbi(PAREDE_ESQUERDA, PAREDE_ESQUERDA, DZ), Zumbi(PAREDE_ESQUERDA, PAREDE_DIREITA-DESLOCAMENTO_ZUMBI, DZ)-DZ, DZ))
+        #MVOER PARA ESQUERDA
+        self.assertEqual(mover_zumbi(Zumbi(PAREDE_DIREITA, PAREDE_DIREITA, DZ), Zumbi(PAREDE_DIREITA-DZ,PAREDE_DIREITA, DZ)))
 
-    unittest.main([teste_moverVaca, teste_moverZumbi])
+    def teste_moverLeite(self):
+
+        #mover leite para cima
+        self.assertEqual(mover_leite(PAREDE_BAIXO, PAREDE_BAIXO, 0), leite(PAREDE_BAIXO, PAREDE_BAIXO-DZ, DZ))
+
+    def teste_moverBala(self):
+
+        #bala para baixo
+        self.assertEqual(mover_bala(PAREDE_CIMA, PAREDE_CIMA, DZZ), bala(PAREDE_CIMA, PAREDE_CIMA+DZZ, DZZ))
+
+    def teste_matarVaca(self):
+
+        #quando o tiro do zumbie acerta a vaca
+        self.assertEqual(colidir_municao(VACA_INICIAL, BALA, DZZ)), colidir_municao(VACA_INICIAL, VACA_INICIAL+DZZ)
+
+    def teste_matarZumbie(self):
+
+        #quando o tiro da vaca acerta o zumbie
+        self.assertEqual(colidir_municao(ZUMBIE_FINAL, LEITE), colidir_municao(ZUMBIE_FINAL, ZUMBIE_FINAL+DZ))
+
+    
+
+
+
+
+
+
+   
+
+
+    #unittest.main()
